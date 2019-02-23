2df6cd86e70575746fe1848f65de6e1209a17142
Udit Sen <udisen97@gmail.com>
2019-02-24 02:15:28 +0530

    Update PPL Program #1.cpp

--- Week 4/PPL Program #1.cpp
+++ Week 4/PPL Program #1.cpp
@@ -1,17 +1,30 @@
 #include <bits/stdc++.h>
-using std::cin;
-using std::cout;
+using namespace std;
 
 int main(){
-    int x;
-    cout << "Enter the number of lines to print: ";
-    cin >> x;
-    int r=0;
-    while(++r<=x){
-    	for(int i=1;i<=r;++i){
-    		cout << i << " ";
-		}
-		cout << std::endl;
+    int n, max, smax;
+    cout << "Enter the count of numbers: ";
+    cin >> n;
+    int a[n];
+    cout << "Enter the integers: \n";
+    for(int i=0; i<n;++i){
+    	cin >> a[i];
+    	if(i==0) max = a[i];
+    	
+		if(a[i] > max) smax = max,max = a[i] ;
+		else if(i==1) smax = a[i];
+		else if(a[i] > smax) smax = a[i];
+		
+	/*
+	else if(i==1) {
+    		if(a[i] > max) smax = max,max = a[i] ;
+    		else smax = a[i];
 	}
-    
+    	else {
+		if(a[i] > max) smax = max,max = a[i] ;
+		else if ( a[i] > smax) smax = a[i];
+	}
+	*/
+    }
+	cout << "Second Maximum number is: " << smax;
 }
